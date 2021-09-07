b605f63ae81a18a6c6f506b8fd780fd57af5c063
leastkids <leastkids@gmail.com>
2021-09-07 20:40:31 +0900

    text input

---
+++ TextInput/TextInput/ContentView.swift
@@ -0,0 +1,71 @@
+//
+//  ContentView.swift
+//  TextInput
+//
+//  Created by 김진홍 on 2021/09/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var userName: String = ""
+    @State private var password: String = ""
+    
+    var body: some View {
+        
+        VStack (spacing : 10){
+            
+            HStack {
+                TextField("사용자이름을 입력해 주세요", text: $userName)
+                    .autocapitalization(.none)
+                    .disableAutocorrection(true)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    
+                
+                Button(action : {
+                    self.userName = ""
+                }) {
+                    
+                    if (self.userName.count > 0) {
+                        Image(systemName: "multiply.circle.fill")
+                            .foregroundColor(Color.secondary)
+                            .font(.system(size : 25))
+                    }
+                    
+                }
+            }
+            
+            HStack {
+                SecureField("비밀번호를 입력해 주세요", text : $password)
+                    .autocapitalization(.none)
+                    .disableAutocorrection(true)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                
+                Button(action : {
+                    self.password = ""
+                }) {
+                    
+                    if(self.password.count > 0) {
+                        Image(systemName: "multiply.circle.fill")
+                            .foregroundColor(Color.secondary)
+                            .font(.system(size : 25))
+                    }
+                    
+                }
+            }
+            
+            
+            
+            Text("입력한 비번 : \(password)")
+        }
+        .padding(.horizontal, 50)
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TextInput/TextInput/TextInputApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextInputApp.swift
+//  TextInput
+//
+//  Created by 김진홍 on 2021/09/07.
+//
+
+import SwiftUI
+
+@main
+struct TextInputApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
